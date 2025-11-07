7a87262303db5c2b89300e7e6e3da35d7e70ce37
lukedale90 <51388504+lukedale90@users.noreply.github.com>
2025-11-07 15:32:41 +0000

    raf woodvale added. Lil Riss spelling mistake fixed

--- src/utils/available-sites.ts
+++ src/utils/available-sites.ts
@@ -8,7 +8,7 @@ export type LocationOption = {
 export const locationOptions: LocationOption[] = [
   { group: "2FTS", name: "RAF Syerston", lat: 53.023333, lng: -0.911667 },
   { group: "2FTS", name: "RAF Topcliffe", lat: 54.205556, lng: -1.382222 },
-  { group: "2FTS", name: "RAF Little Risington", lat: 51.866667, lng: -1.7 },
+  { group: "2FTS", name: "RAF Little Rissington", lat: 51.866667, lng: -1.7 },
   { group: "2FTS", name: "RAF Kenley", lat: 51.303611, lng: -0.095 },
   { group: "2FTS", name: "RAF Honington", lat: 52.3425, lng: 0.773056 },
   {
@@ -24,7 +24,12 @@ export const locationOptions: LocationOption[] = [
     lat: 49.999167,
     lng: -5.230556,
   },
-
+  {
+    group: "2FTS",
+    name: "RAF Woodvale",
+    lat: 53.581667,
+    lng: -3.055556,
+  },
   { group: "RAFGSA", name: "Colerne", lat: 51.439167, lng: -2.286389 },
   { group: "RAFGSA", name: "Cranwell", lat: 53.0425, lng: -0.496 },
   { group: "RAFGSA", name: "Easterton", lat: 57.586496, lng: -3.321363 },
